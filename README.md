03b90db0e71174d2b5c5e329bd99d5258bfbb307
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-07-23 19:27:04 +0300

    feat: add modification and library files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pro-scss-opencart
 PRO SCSS
+
+Replace default scss library with up to date (scssphp)[https://github.com/scssphp/scssphp]
---
+++ system/library/pro_scss/pro_scss.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace pro_scss;
+require_once __DIR__ . '/vendor/autoload.php';
+
+class pro_scss extends \ScssPhp\ScssPhp\Compiler
+{}
